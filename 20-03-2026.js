861e4cf0960e3c2dbfd947b72c7b18391e0fee0c
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-03-20 23:28:49 +0200

    Naive implementation for challenge 20-03-2026.js

---
+++ 20-03-2026.js
@@ -0,0 +1,28 @@
+// This is the initial naive implementation, which I will optimise later.
+
+function maxSubArray(array, k) {
+  let start = 0;
+  let end = start + k;
+  const results = [];
+
+  while (end <= array.length) {
+    const subArray = array.slice(start, end);
+    const maxElement = max(subArray);
+    results.push(maxElement);
+    start++;
+    end++;
+  }
+
+  return results;
+}
+
+
+function max(nums) {
+  let max = Number.NEGATIVE_INFINITY;
+
+  for (const num of nums) {
+    if (num > max) max = num;
+  }
+
+  return max;
+}
