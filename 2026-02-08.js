4fc882a3bff4fae594e52b9648b5b108c3a86b2e
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-03-21 23:18:13 +0200

    Update 2026-02-08.js

--- 2026-02-08.js
+++ 2026-02-08.js
@@ -1,10 +1,17 @@
+// function rearrange(array, order) {
+//   const results = [];
+
+//   for (const index of order) {
+//     const element = array[index];
+//     results.push(element);
+//   }
+
+//   return results;
+// }
+
 function rearrange(array, order) {
-  const results = [];
-
-  for (const index of order) {
-    const element = array[index];
-    results.push(element);
+  for (let i = 0; i < order.length; i++) {
+    order[i] = array[order[i]];
   }
-
-  return results;
+  return order
 }
