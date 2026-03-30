598fd040ec40e4e33cbd41a696259cafee26b0a5
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-03-30 22:56:09 +0200

    Rearranges the elements of an array based on a provided order array.
    Uncomment the for loop in the rearrange function.

--- 2026-02-08.js
+++ 2026-02-08.js
@@ -1,7 +1,7 @@
 // function rearrange(array, order) {
 //   const results = [];
 
-//   for (const index of order) {
+//   for (const index of order) { 
 //     const element = array[index];
 //     results.push(element);
 //   }
